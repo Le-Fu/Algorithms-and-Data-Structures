8a25286e9240ec1a2444bfa354e96ac610fa434e
Meng <ssmdmsn@hotmail.com>
2019-07-03 12:13:53 +0800

    add(leetcode): add reverseNum

--- array/readme.md
+++ array/readme.md
@@ -1,3 +1,20 @@
 # JS Array
-创建一个记录学生成绩的对象，提供一个添加成绩的方法，以及一个显示学生平均成绩
-的方法。
+## 概念
+js中的数组严格来说应该称作对象，是特殊的js对象，在内部被归类为数组。
+
+## 创建数组的几种方式
+1. `var arr = []; `
+2. `var arr = new Array(args)`, 其中`args`可以是一组元素`1,2,3`作为数组的初始化数据, 也可以是单个元素`3`，作为数组的长度。
+
+## 如何判断数组
+在js中判断数组类型其实并不容易，因为：`typeof arr === 'object'`。好在es6数组的新方法`Array.isArray()`解决了这一问题。
+那么，没有这个方法前我们是如何判断一个变量是不是数组呢？
+```
+var isType = function(type) {
+  return function(x) {
+    return Object.prototype.toString.call(x).toLocaleLowerCase() === '[object '+ type.toLocaleLowerCase() + ']';
+  } 
+} 
+
+var isArray = isType('array')
+```
--- leetcode/reverseNum.js
+++ leetcode/reverseNum.js
@@ -1,17 +1,27 @@
-var reverse = function(x) {
-  var iltz = x < 0 ? true : false,
-      str = '',
-      xArr = (x+'').split('')
-  if (iltz) {
-      xArr.shift()
-  }
-  for (var i = xArr.length-1; i>=0; i--) {
-      str += xArr[i]       
-  }
-  if(Number(str)>(2**31-1)){
+// var reverse = function(x) {
+//   var iltz = x < 0 ? true : false,
+//       str = '',
+//       xArr = (x+'').split('')
+//   if (iltz) {
+//       xArr.shift()
+//   }
+//   for (var i = xArr.length-1; i>=0; i--) {
+//       str += xArr[i]       
+//   }
+//   if(Number(str)>(2**31-1)){
+//     return 0
+//   }
+//   return iltz ? '-'+ str : str
+// };
+
+
+var reverse = function (x) {
+  var reverseNum = (function (x) {
+    return parseInt((x + '').split('').reverse().join(''))
+  })(x)
+  if (reverseNum > 2 ** 31 - 1) {
     return 0
   }
-  return iltz ? '-'+ str : str
-};
-
-console.log(reverse(-1234566789));
+  return x > 0 ? reverseNum : -1 * reverseNum
+}
+console.log(reverse(12304));
