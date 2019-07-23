040580a2e2230938bd13f35e32a2db08ff3698a4
Meng <ssmdmsn@hotmail.com>
2019-07-23 19:15:16 +0800

    add(leetcode): strSrc

---
+++ leetcode/strStr.js
@@ -0,0 +1,9 @@
+// https://www.yuque.com/db/zn6g8d/leetcode-20190723
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+};
